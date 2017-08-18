7002ac1f6ac040e8674cc08b0fd30a456492a734
MiguelBrito1086 <mbrito1086@gmail.com>
2017-08-18 13:38:22 -0400

    Finished

--- src/components/App.js
+++ src/components/App.js
@@ -24,30 +24,12 @@ class App extends Component {
     const pilot = {
       pilot: this.state.pilot
     }
-    // const pilot = this.state.pilot
-    // pilot.push(piloting)
-    //
+    const pilots = this.state.pilot
+
     // this.setState({
     //   pilot: ""
     // })
   }
-
-  // LIFECYCLE
-  // Which lifecycle is best for fetching data?
-  // Inside this lifecycle, you will fetch the vehicles from here: https://swapi.co/api/vehicles/
-  // Once you have fetched that data, set the state of vehicles to the fetched data.
-  // In your response look for 'results'. It should return this array.
-  // You will want to use this array when you set the state of 'vehicles'. You will need this data in your render.
-  // Enter your code below:
-
-
-  // RENDER
-  // Before you can map over the data you've fetched, you will first need to store that 'state' in a variable.
-  // Map over the data.
-  // Don't forget to set the 'key'. In this case, use the vehicle name.
-  // You will need the following values: name, model, manufacturer, class, passengers, crew, length, max speed, and cargo capacity.
-  // Rendering: create a 'card' for each of the vehicles. consult the Bootstrap 4 docs for details.
-  // Enter your code below:
 
   render() {
       return (
